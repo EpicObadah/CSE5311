4b5e58c9ee3c8cb38c26348bc0d99895c95f443d
EpicObadah <alsmadiobadah@gmail.com>
2025-01-28 20:58:03 -0600

    Completed handson2

---
+++ Handson2/INSERTION.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void insertion_sort(int arr[], int n);
+
+int main()
+{
+    int n = 250000; 
+    int min = 1;
+    int max = 100;
+    double elapsedTime = 0;
+    clock_t start;
+    clock_t end;
+
+    srand(time(0));
+
+    int* arr = (int*)malloc(n*sizeof(int));
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rand() % (max-min+1) + min;
+    }
+/*
+    printf("Unsorted Array : \n");
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+*/
+    printf("\n");
+
+    start = clock();
+
+    insertion_sort(arr, n);
+
+    end = clock();
+/*
+    printf("Insertion Sorted Array : \n");
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+*/
+    elapsedTime = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("\n\nelapsed time : %.6f seconds\n", elapsedTime);
+
+    free(arr);
+    return 0;
+
+}
+
+void insertion_sort(int arr[], int n)
+{
+    for(int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && arr[j] > key)
+        {
+            arr[j+1] = arr[j]; //swap
+            j--;
+        }
+
+        arr[j+1] = key;
+    }
+}
+
---
+++ Handson2/SELECTION.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void selection_sort(int arr[], int n);
+
+int main()
+{
+    int n = 250000; 
+    int min = 1;
+    int max = 100;
+    double elapsedTime = 0;
+    clock_t start;
+    clock_t end;
+
+    srand(time(0));
+
+    int* arr = (int*)malloc(n*sizeof(int));
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rand() % (max-min+1) + min;
+    }
+/*
+    printf("Unsorted Array : \n");
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+*/
+    printf("\n");
+
+    start = clock();
+
+    selection_sort(arr, n);
+
+    end = clock();
+/*
+    printf("Insertion Sorted Array : \n");
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+*/
+    elapsedTime = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("\n\nelapsed time : %.6f seconds\n", elapsedTime);
+
+    free(arr);
+    return 0;
+    
+}
+
+void selection_sort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min = i;
+    
+
+        for(int j= i+1; j<n; j++)
+        {
+            if(arr[j] < arr[min])
+            {
+                 min = j;
+            }
+        }
+
+        if(min != i)
+        {
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+}
