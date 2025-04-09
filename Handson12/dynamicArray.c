d62511fac1e84aa2b07a768ed31202fe95c96949
EpicObadah <alsmadiobadah@gmail.com>
2025-04-08 23:51:09 -0500

    Added deletion function

--- Handson12/dynamicArray.c
+++ Handson12/dynamicArray.c
@@ -11,6 +11,7 @@ typedef struct
 void resizeArray(DynamicArray *arr);
 void addElement(DynamicArray *arr, int element);
 int getElement(DynamicArray *arr, int index);
+void deleteElement(DynamicArray *arr, int index);
 
 int main()
 {
@@ -19,6 +20,8 @@ int main()
     arr.size = 0;
     arr.capacity = 2;
 
+    printf("\nInserting elements into the dynamic array which is currently empty.\n\n");
+
     addElement(&arr, 11);
     addElement(&arr, 19);
     addElement(&arr, 100);
@@ -26,10 +29,22 @@ int main()
     addElement(&arr, 3);
     addElement(&arr, 1);
 
+    printf("Array after inserting 6 elements\n");
+
     for(int i = 0; i < arr.size; i++)
     {
         printf("Element at index %d: %d\n", i, getElement(&arr,i));
     }
+
+    deleteElement(&arr, 1);
+
+    printf("\n\nArray after deleting the element at index 1\n");
+
+    for(int i = 0; i < arr.size; i++)
+    {
+        printf("Element at index %d: %d\n", i, getElement(&arr,i));
+    }
+
 
     free(arr.array);
     arr.array = NULL;
@@ -70,3 +85,18 @@ int getElement(DynamicArray *arr, int index)
     return arr->array[index];
 }
 
+void deleteElement(DynamicArray *arr, int index)
+{
+    if(index < 0 || index >= arr->size)
+    {
+        printf("Index out of bounds\n");
+        exit(1);
+    }
+
+    for(int i = index; i < arr->size - 1; i++)
+    {
+        arr->array[i] = arr->array[i+1];
+    }
+
+    arr->size--;
+}
